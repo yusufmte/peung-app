b1873aa3f2e3b7b6ba600245481bce9c77c7b1df
Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
2025-07-21 16:42:01 -0400

    set up audio recording
    This works out the details of creating the `AudioStreamPlayer`s,
    creating the recording bus and effect, and linking things up.
    As an example, after recording it just plays back the recorded clip.

--- scripts/audio_manager.gd
+++ scripts/audio_manager.gd
@@ -1,8 +1,29 @@
 extends Control
 
+var recording_effect : AudioEffect
+var recording
+var mic
+var player
+
 func _ready():
 	var return_button = $ColorRect/MarginContainer/VBoxContainer/ReturnButtonMargin/ReturnButton
 	return_button.pressed.connect(_on_ReturnButton_pressed)
+	recording_effect = AudioServer.get_bus_effect(AudioServer.get_bus_index("Record"), 0)
+	mic = $RecordingAudioStream
+	player = $RecordingPlaybackAudioStream
 
 func _on_ReturnButton_pressed():
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+func _on_record_button_pressed() -> void:
+	if recording_effect.is_recording_active():
+		recording = recording_effect.get_recording()
+		mic.stop()
+		recording_effect.set_recording_active(false)
+
+		# Play back the recorded thing to demo how to get the stream and play it
+		player.stream = recording
+		player.play()
+	else:
+		mic.play()
+		recording_effect.set_recording_active(true)
