eb4b7ec7b9d1c3696769f73ddedf1427e1b3f3b8
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2025-07-24 14:15:59 -0400

    link audio buttons enabledness to selection

--- scripts/audio_manager.gd
+++ scripts/audio_manager.gd
@@ -19,6 +19,8 @@ func _ready() -> void:
 	for key in Global.sound_keys():
 		item_list.add_item(key)
 
+	_update_buttons_enabledness()
+
 func _on_ReturnButton_pressed():
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
 
@@ -36,3 +38,21 @@ func _on_record_button_pressed() -> void:
 		mic.play()
 		recording_effect.set_recording_active(true)
 		$%RecordButton.set_icon_to_stop()
+
+func _update_buttons_enabledness() -> void:
+	var nothing_selected : bool = not %AudioManagerItemList.is_anything_selected()
+	%RecordButton.disabled = nothing_selected
+	%PlayButton.disabled = nothing_selected
+
+	if nothing_selected:
+		%DeleteButton.disabled = true
+	else:
+		var selected_items : PackedInt32Array = %AudioManagerItemList.get_selected_items()
+		if selected_items.size() != 1:
+			%DeleteButton.disabled = true
+		else:
+			var sound_key : String = %AudioManagerItemList.get_item_text(selected_items[0])
+			%DeleteButton.disabled = not FileAccess.file_exists(Global.get_user_sound_path(sound_key))
+
+func _on_audio_manager_item_list_item_selected(index: int) -> void:
+	_update_buttons_enabledness()
